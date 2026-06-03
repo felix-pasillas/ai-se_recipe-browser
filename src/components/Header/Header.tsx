f86cb714ede9b1583eacb31e3d710f88c8c39e30
Kevin Loughead <kvnloughead@gmail.com>
2026-06-03 15:13:54 -0400

    update starting code styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
+import Logo from "../../assets/logo.svg";
 import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
       <div className="header__inner">
-        <span className="header__logo">Recipe Browser</span>
+        <img className="header__logo" src={Logo} alt="Recipe Browser logo" />
       </div>
     </header>
   );
--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -1,4 +1,6 @@
 import type { Recipe } from "../../types";
+import { categoryColors } from "../../data/recipes";
+
 import "./RecipeCard.css";
 
 type Props = {
@@ -8,7 +10,14 @@ type Props = {
 function RecipeCard({ recipe }: Props) {
   return (
     <article className="recipe-card">
-      <span className="recipe-card__category">{recipe.category}</span>
+      <span
+        style={{
+          backgroundColor: categoryColors[recipe.category.toLocaleLowerCase()],
+        }}
+        className="recipe-card__category"
+      >
+        {recipe.category}
+      </span>
       <h2 className="recipe-card__title">{recipe.title}</h2>
       <p className="recipe-card__description">{recipe.description}</p>
     </article>
